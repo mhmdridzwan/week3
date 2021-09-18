9efe58c75f2fcbe7a025341a2a005857e13bc3c1
mhmdridzwan <89780325+mhmdridzwan@users.noreply.github.com>
2021-09-18 15:51:27 +0800

    Update new.js

--- new.js
+++ new.js
@@ -151,6 +151,12 @@ for (var i = 0; i < lines; i++)
 var columns = 13;
 for ( var i = 0; i < columns; i++){
   for (var j = 0; j < columns; j++) {
-    document.write("#");
+    if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1) {
+      document.write("#")
+    }
+    else {
+      document.write("0")
+    }
   }
+  document.write("<br/>")
 }
